fa3b6c8bca95bb38b26fe1c99ee8961848d84ebc
heymahh <heymahh@gmail.com>
2024-06-12 17:17:22 -0300

    START

---
+++ src/others/brazilbanks_icons.dart
@@ -0,0 +1,38 @@
+// Place fonts/brazilbanks.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: brazilbanks
+//      fonts:
+//       - asset: fonts/brazilbanks.ttf
+import 'package:flutter/widgets.dart';
+
+class Brazilbanks {
+  Brazilbanks._();
+
+  static const String _fontFamily = 'brazilbanks';
+
+  static const IconData banrisul = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData bb = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData bmg = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData bradesco = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData brb = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData btg = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData c6 = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData caixa = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData digio = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData inter = IconData(0xe91f, fontFamily: _fontFamily);
+  static const IconData itau = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData neon = IconData(0xe950, fontFamily: _fontFamily);
+  static const IconData next = IconData(0xe928, fontFamily: _fontFamily);
+  static const IconData nubank = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData original = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData others = IconData(0xe932, fontFamily: _fontFamily);
+  static const IconData picpay = IconData(0xe939, fontFamily: _fontFamily);
+  static const IconData safra = IconData(0xe93e, fontFamily: _fontFamily);
+  static const IconData santander = IconData(0xe940, fontFamily: _fontFamily);
+  static const IconData sicoob = IconData(0xe942, fontFamily: _fontFamily);
+  static const IconData sicredi = IconData(0xe946, fontFamily: _fontFamily);
+  static const IconData votorantim = IconData(0xe949, fontFamily: _fontFamily);
+  static const IconData xp = IconData(0xe94e, fontFamily: _fontFamily);
+}
